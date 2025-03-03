3f492263a0cb73fccfe4cf57cf0d6d5a1e684d51
Adityaverma008 <Adityaverma008@users.noreply.github.com>
2025-03-03 12:02:45 +0530

    [Aditya Verma] adding UC1setup file

--- src/test/java/com/example/EmployeePayrollApp/EmployeePayrollAppApplicationTests.java
+++ src/test/java/com/example/EmployeePayrollApp/EmployeePayrollAppApplicationTests.java
@@ -1,4 +1,4 @@
-package com.bridgelabz.employeepayrollapp;
+package com.example.EmployeePayrollApp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
